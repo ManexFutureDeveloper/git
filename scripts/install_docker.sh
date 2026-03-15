42ee8fc9945852524a3b7116e4dd6554581a889f
ManexDev <143217759+ManexFutureDeveloper@users.noreply.github.com>
2026-03-15 14:58:49 +0100

    Update install_docker.sh

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -1,48 +1,39 @@
 #!/bin/bash
-# install_docker.sh - Instalación de Docker Engine y Docker Compose
-# Compatibilidad: Ubuntu / Debian
-
-#!/bin/bash
-
 set -e
 
 echo "=== Actualizando sistema ==="
-sudo apt update && sudo apt upgrade -y
+apt update && apt upgrade -y
 
 echo "=== Instalando dependencias ==="
-sudo apt install -y 
-ca-certificates 
-curl 
-gnupg 
-lsb-release 
-apt-transport-https 
-software-properties-common
+apt install -y \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release \
+  apt-transport-https \
+  software-properties-common
 
 echo "=== Docker GPG key ==="
-sudo install -m 0755 -d /etc/apt/keyrings
+install -m 0755 -d /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | 
-sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/debian/gpg | \
+  gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 
-echo "===  Añadiendo repositorio docker ==="
-echo 
-"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] 
-https://download.docker.com/linux/ubuntu 
-$(lsb_release -cs) stable" | 
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "=== Añadiendo repositorio Docker ==="
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo "=== Instalando Docker ==="
-sudo apt update
-sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-echo "=== Activando el inicio automatico ==="
-sudo systemctl enable docker
-sudo systemctl start docker
+echo "=== Activando el inicio automático ==="
+systemctl enable docker
+systemctl start docker
 
 echo "=== Comprobando que Docker funciona ==="
 docker --version
 docker compose version
-
-
